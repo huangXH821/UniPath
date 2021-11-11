e0abf1e2b9129f5ac4b4e68b536caa4e97486fc9
Shazia6 <deanwinchester6661@gmail.com>
2021-11-11 12:01:14 -0500

    Add files via upload

---
+++ java/com/company/exchange/dao/CommentsMapper.java
@@ -0,0 +1,24 @@
+package com.company.exchange.dao;
+
+import com.company.exchange.pojo.Comments;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface CommentsMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Comments record);
+
+    int insertSelective(Comments record);
+
+    Comments selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Comments record);
+
+    int updateByPrimaryKeyWithBLOBs(Comments record);
+
+    int updateByPrimaryKey(Comments record);
+
+    @Select("select count(1) from comments where goods_id=#{goodsId}")
+    int getCommentCount(@Param("goodsId") Integer goodsId); //括号里面是什么？@param？
+}
